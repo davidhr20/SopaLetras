9d4967f6391390208f66d824a71bba7914f2b3d6
Hp <Hp@LAPTOP-E83T45HN>
2019-01-10 21:04:03 +0100

    Version 0.0

---
+++ src/SopaLetras.java
@@ -0,0 +1,17 @@
+/** JUEGO DEL TRES EN RAYA 
+ ** Autor: David Hierro Romo 
+ ** Fecha: 8 de enero de 2019
+ ** Versión: 1.0 
+ ** Descripción: sopa de letras de 15x15
+ **/
+
+public class SopaLetras {
+
+	private char [] [] matriz = {{'a','b','c','d','e'} , {'f','g','h','i','j'}};
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
